4cf138e9d37e51b437d68cd0ea3b8ba9a51f7d0f
tdavis3 <tyronedavisci@gmail.com>
2019-05-23 21:21:57 -0400

    updated stack and queue

--- src/hw_1/part_3/Queue.java
+++ src/hw_1/part_3/Queue.java
@@ -4,39 +4,58 @@ import java.util.ArrayList;
 
 public class Queue {
 
+    private Integer min;
     private Integer size;
     private ArrayList<Integer> queue;
 
-    public Queue() {
+    private Queue() {
         queue = new ArrayList<>();
         size = 0;
     }
 
-    public void enqueue(Integer to_enqueue) {
+    private void enqueue(Integer to_enqueue) {
+        if (min == null) {
+            min = to_enqueue;
+        } else if (to_enqueue < min) {
+            min = to_enqueue;
+        }
         queue.add(0, to_enqueue);
         size += 1;
     }
 
-    public Integer dequeue() {
+    private Integer dequeue() {
         if (this.isEmpty()) {
             throw new IllegalCallerException("The stack is empty. Cannot pop anything off the top.");
         }
         Integer val = queue.get(queue.size() - 1);
         queue.remove(queue.size() - 1);
         size -= 1;
+        if (val.equals(min)) {
+            Integer new_minimum = null;
+            for (Integer num : queue) {
+                if (new_minimum == null) {
+                    new_minimum = num;
+                } else if (num < new_minimum) {
+                    new_minimum = num;
+                }
+            }
+            min = new_minimum;
+        }
         return val;
     }
 
-    public Integer size() {
+    private Integer size() {
         return size;
     }
 
-    public Boolean isEmpty() {
+    private Boolean isEmpty() {
         if (size == 0) {
             return Boolean.TRUE;
         }
         return Boolean.FALSE;
     }
 
-//    TODO Implement min()
+    private Integer min() {
+        return min;
+    }
 }
--- src/hw_1/part_3/Stack.java
+++ src/hw_1/part_3/Stack.java
@@ -2,43 +2,75 @@ package hw_1.part_3;
 
 import java.util.ArrayList;
 
+
 public class Stack {
 
+    private Integer min;
     private Integer size;
     private ArrayList<Integer> stack;
 
-    public Stack() {
+    private Stack() {
         stack = new ArrayList<>();
         size = 0;
+        min = null;
     }
 
-    public void push(Integer to_top) {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(42);
+        stack.push(9000);
+        stack.push(50);
+        System.out.println(stack.min());
+        System.out.println(stack.pop());
+    }
+
+    private void push(Integer to_top) {
+        if (min == null) {
+            min = to_top;
+        } else if (to_top < min) {
+            min = to_top;
+        }
         stack.add(0, to_top);
         size += 1;
     }
 
-    public Integer pop() {
+    private Integer pop() {
         if (this.isEmpty()) {
             throw new IllegalCallerException("The stack is empty. Cannot pop anything off the top.");
         }
         Integer val = stack.get(0);
         stack.remove(0);
         size -= 1;
+        if (val.equals(min)) {
+            Integer new_minimum = null;
+            for (Integer num : stack) {
+                if (new_minimum == null) {
+                    new_minimum = num;
+                } else if (num < new_minimum) {
+                    new_minimum = num;
+                }
+            }
+            min = new_minimum;
+        }
         return val;
     }
 
-    public Integer top() {
+    private Integer top() {
         return stack.get(0);
     }
 
-    public Integer size() {
+    private Integer size() {
         return size;
     }
-    
-    public Boolean isEmpty() {
+
+    private Boolean isEmpty() {
         if (size == 0) {
             return Boolean.TRUE;
         }
         return Boolean.FALSE;
+    }
+
+    private Integer min() {
+        return min;
     }
 }
